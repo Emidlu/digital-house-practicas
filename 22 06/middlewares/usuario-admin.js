b06fa4039821904070a60105286898a9d0091f3f
Emidlu <52872122+Emidlu@users.noreply.github.com>
2020-07-04 00:42:14 -0300

    22 06 fixed

--- 22 06/middlewares/usuario-admin.js
+++ 22 06/middlewares/usuario-admin.js
@@ -1,7 +1,7 @@
 module.exports = function(req, res, next){
   let admins = ['Ada', 'Greta', 'Vim', 'Tim'];
 
-  if(!req.query.user || !admins.includes(req.query.user){
+  if(!req.query.user || !admins.includes(req.query.user)){
     res.send('No tenes los privilegios');
   }
 
--- 22 06/routes/index.js
+++ 22 06/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+
 let usuarioAdmin = require('../middlewares/usuario-admin');
 
 /* GET home page. */
