0f9b3e42da5e1dc1c26d7be7d0811b3abace3580
Emidlu <52872122+Emidlu@users.noreply.github.com>
2020-07-15 22:17:35 -0300

    15 07 primera version

---
+++ 15 07/CRUD/routes/peliculas.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var router = express.Router();
+var peliculasController = require("../controllers/peliculasController")
+
+router.get("/crear", peliculasController.crear);
+
+module.exports = router;
